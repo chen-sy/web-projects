7ed853e108744145e407243a359fdd503d29f921
chensy <724060052@qq.com>
2018-09-07 17:30:02 +0800

    第二次提交

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,19 @@
-function fun(){
-    var i=0;
-    return "";
-}
+/**
+ * 添加个性化的欢迎信息
+ */
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+function SetUserName() {
+    var myName=prompt('Please enter your name.');
+    localStorage.setItem('name',myName);
+    myHeading.textContent='Hi,'+myName;
+  }
+  if(!localStorage.getItem('name')) {
+    SetUserName();
+  } else {
+    var storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Mozilla is cool, ' + storedName;
+  }
+  myButton.onclick = function() {
+    SetUserName();
+  }
