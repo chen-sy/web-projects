1e9807f57b5da688d78602a5d5e8a2a6e88efbd4
Administrator <Administrator@MININT-F36S5EH>
2018-11-12 13:19:01 +0800

    测试

---
+++ WebCCMS/WebCCMS/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebCCMS.Startup))]
+namespace WebCCMS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
